4e8004746fba8916f020a88c7ed1bad0858548a7
Marissol Karczeski <marfkar@gmail.com>
2020-05-07 12:23:19 -0700

    add exercise code

--- public/exercise.js
+++ public/exercise.js
@@ -17,20 +17,42 @@ const newWorkout = document.querySelector(".new-workout")
 let workoutType = null;
 let shouldNavigateAway = false;
 
-async function initExercise() {
-  let workout;
 
-  if (location.search.split("=")[1] === undefined) {
-    workout = await API.createWorkout()
-    console.log(workout)
-  }
+init();
+
+  async function init() {
+    if (location.pathname.includes("/exercise") && location.search.split("=")[1] === undefined) {
+      console.log("excersise")
+      const newWorkout = await API.createWorkout();
+      const workout = await API.getLastWorkout();
+      if (workout) {
+        location.search = "?id=" + workout._id;
+      }
+      else {
+        newWorkout.classList.add("")
+      }
+      return console.log(newWorkout);
+    }
+    if (location.search.split("=")[1] === undefined) {
+      const workout = await API.getLastWorkout();
+      console.log(workout)
+
   if (workout) {
     location.search = "?id=" + workout._id;
   }
-
+  else {
+    newWorkout.classList.add("")
 }
+    }
+  }
 
-initExercise();
+// initExercise();
+
+
+
+
+
+
 
 function handleWorkoutTypeChange(event) {
   workoutType = event.target.value;
